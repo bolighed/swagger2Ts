58cbc7ececcc148866886085d13c02470076ef07
Pachito Marco Calabrese <pm.calabrese@gmail.com>
2017-12-07 10:33:57 +0100

    remove example

--- example/example.js
+++
@@ -1,9 +0,0 @@
-const s2ts = require('../index.js');
-const path = require('path');
-const CONFIG = {
-    api_url: 'http://localhost:18000/api/schema/1.0/?format=openapi',
-    swagger_file: path.resolve(__dirname, './api_schema.json'),
-    interfaces_dist_folder: path.resolve(__dirname, './src/interfaces/')
-}
-
-s2ts(CONFIG);
